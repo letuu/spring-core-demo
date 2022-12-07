1730ba87272f919d92f7dd05f8af7289c629d831
letuu <albertvt@ya.ru>
2022-12-07 21:58:26 +0300

    Initial commit

---
+++ src/main/java/pro/sky/springcore/SpringCoreDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.springcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCoreDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/springcore/SpringCoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.springcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
